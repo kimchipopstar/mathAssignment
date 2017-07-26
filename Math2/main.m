7580cef8660887ced584e47b0c68dc4ccfcaade6
J.K <K_940201@hotmail.com>
2017-07-25 17:51:19 -0700

    Initial Commit

---
+++ Math2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Math2
+//
+//  Created by Jaewon Kim on 2017-07-25.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
